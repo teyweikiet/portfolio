4484759942364ace6dc56a2b4faa1140cd6bb7bd
TWK <teyweikiet@gmail.com>
2020-06-19 16:22:19 +0800

    1. scrolling properly 2. alignment of everything seems right

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let scrollY = 0,
-    body = document.querySelector("body"),
-    scrollable = document.querySelector('#scrollable'),
-    isAnimating = false;
+// let scrollY = 0,
+let body = document.querySelector("body");
+    // scrollable = document.querySelector('#scrollable'),
+    // isAnimating = false;
     // isWheeling = False,
     
-frame = document.getElementById("frame");
+// frame = document.getElementById("frame");
 
 // toggleMenu = (btn) => {document.querySelector('header').classList.toggle('change');}
 
@@ -12,59 +12,43 @@ toggleTheme = () => {
     body.classList.toggle('dark');
 }
 
-document.addEventListener('drag', (e) => {
-    // body.style.overflow = 'hidden';
-    console.log(window.scrollY);
-    // if (window.scrollY > scrollY) {
-    //     scrollY += 100;
-    //     frame.style.top = -1 * scrollY + `vh`;
-    // }
-    // body.style.overflow = 'auto';
+let yPos = 0,
+  isAnimating = false,
+  frame = document.querySelector("#frame"),
+  scrollable = document.querySelector("#scrollable");
+
+document.addEventListener("wheel", (e) => {
+  if (!isAnimating) {
+    if (e.deltaY > 0) {
+      customScroll(1);
+    } else if (e.deltaY < 0) {
+      customScroll(-1);
+    }
+  }
 });
 
-document.addEventListener('wheel', (e) => {
-    console.log('wheel');
+let customScroll = (dir) => {
+  isAnimating = true;
 
-    if (!isAnimating) {
-        if (e.deltaY < 0) {
-            console.log(-1);
-            customScroll(-1);
-        } else if (e.deltaY > 0) {
-            console.log(1);
-            customScroll(1);
-        }
-    }
-    
-    // if (event.deltaY < 0) {
-    //     // console.log('scrolling up');
-    //     customScroll(1);
-    // } else if (event.deltaY > 0) {
-    //     // console.log('scrolling down');
-    //     customScroll(-1);
-    // }
-})
+  if (dir == 1) {
+    yPos -= 100;
+  } else {
+    yPos += 100;
+  }
 
-customScroll = (dir) => {
-    isAnimating = true;
-
-    if (dir == 1) {
-        scrollY -= 100;
-    } else if (dir == -1) {
-        scrollY += 100;
-    }
-
-    console.log(scrollY);
-    scrollY = Math.min(Math.max(scrollY, -200), 0);
-
-    frame.style.top = scrollY + `vh`;
-    // if (!isAnimating) {
-    //     scrollY += 100;
-    //     frame.style.top = -1 * scrollY + `vh`;
-    //     isAnimating = true;
-    // } 
-}
-
-body.addEventListener('transitionend', () => {
+  if (yPos < -300) {
+    yPos = -300;
     isAnimating = false;
-    console.log(isAnimating);
+    return;
+  } else if (yPos > 0) {
+    yPos = 0;
+    isAnimating = false;
+    return;
+  }
+  scrollable.style.top = yPos + "vh";
+};
+
+scrollable.addEventListener("transitionend", () => {
+  console.log("ended");
+  isAnimating = false;
 });
